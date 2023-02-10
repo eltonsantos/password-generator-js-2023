2261343f5452a3c0753e3fa83de18fc3dec4f422
Elton Santos <eltonaxl@hotmail.com>
2023-02-10 14:11:30 -0300

    Add strong password

--- scripts.js
+++ scripts.js
@@ -6,7 +6,8 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!";
+let charset =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?;%#&<>+_-,.";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
